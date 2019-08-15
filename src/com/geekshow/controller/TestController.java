5a768566753b140c29defe14700323c6a0dd572f
darth-xiaozhijun <15717061273@163.com>
2019-08-15 21:57:58 +0800

    使用注解方式实现springmvc

---
+++ src/com/geekshow/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.geekshow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("demo")
+	public String demo(){
+		System.out.println("执行demo");
+		return "main.jsp";
+	}
+	@RequestMapping("demo2")
+	public String demo2(){
+		System.out.println("demo2");
+		return "main1.jsp";
+	}
+}
