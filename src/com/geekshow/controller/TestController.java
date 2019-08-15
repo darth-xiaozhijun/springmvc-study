b9b98a3e9b46dba62b4767f965679b590d172ba3
darth-xiaozhijun <15717061273@163.com>
2019-08-15 23:00:21 +0800

    传值

--- src/com/geekshow/controller/TestController.java
+++ src/com/geekshow/controller/TestController.java
@@ -1,7 +1,14 @@
 package com.geekshow.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.geekshow.pojo.Demo;
+import com.geekshow.pojo.People;
 
 @Controller
 public class TestController {
@@ -13,9 +20,60 @@ public class TestController {
 		return "main.jsp";
 	}
 	
+	@RequestMapping("demo1")
+	public String demo1(@RequestParam(value="name1") String name,
+			@RequestParam(value="age1")int age){
+		
+		System.out.println("执行demo"+" "+name+" "+age);
+		return "main.jsp";
+	}
+	
+	@RequestMapping("page")
+	public String page(@RequestParam(defaultValue="2")int pageSize,
+			@RequestParam(defaultValue="1") int pageNumber){
+		
+		System.out.println(pageSize+" "+pageNumber);
+		return "main.jsp";
+	}
+	
 	@RequestMapping("demo2")
-	public String demo2(){
-		System.out.println("demo2");
+	public String demo2(@RequestParam(required=true)String name){
+		System.out.println("name 是SQL 的查询条件,必须要传递name 参数"+name);
+		return "main.jsp";
+	}
+	
+	@RequestMapping("demo3")
+	public String demo3(@RequestParam(required=true,defaultValue="测试")String name){
 		return "main1.jsp";
+	}
+
+	@RequestMapping("demo4")
+	public String demo4(People peo){
+		return "main.jsp";
+	}
+	
+	@RequestMapping("demo5")
+	public String demo5(String name,int age,@RequestParam("hover") List<String> abc){
+		System.out.println(name+" "+age+" "+abc);
+		return "main.jsp";
+	}
+	
+	@RequestMapping("demo6")
+	public String demo6(Demo demo){
+		System.out.println(demo);
+		return "main.jsp";
+	}
+	
+	@RequestMapping("demo7")
+	public String demo7(String name,int age){
+		
+		System.out.println("执行demo"+" "+name+" "+age);
+		return "main.jsp";
+	}
+	
+	@RequestMapping("demo8/{id1}/{name}")
+	public String demo8(@PathVariable String name,@PathVariable("id1") int age){
+	System.out.println(name +" "+age);
+	return "/main.jsp";
 	}
 }
---
+++ src/com/geekshow/pojo/Demo.java
@@ -0,0 +1,37 @@
+package com.geekshow.pojo;
+
+import java.util.List;
+
+public class Demo {
+
+	/*private People peo;
+
+	public People getPeo() {
+		return peo;
+	}
+
+	public void setPeo(People peo) {
+		this.peo = peo;
+	}
+
+	@Override
+	public String toString() {
+		return "Demo [peo=" + peo + "]";
+	}*/
+	
+	private List<People> peo;
+
+	public List<People> getPeo() {
+		return peo;
+	}
+
+	public void setPeo(List<People> peo) {
+		this.peo = peo;
+	}
+
+	@Override
+	public String toString() {
+		return "Demo [peo=" + peo + "]";
+	}
+	
+}
