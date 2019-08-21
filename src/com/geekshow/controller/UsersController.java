8f2841cf080d21e45fbdd9746f602e779ff7da45
darth-xiaozhijun <15717061273@163.com>
2019-08-21 23:34:10 +0800

    登录拦截

--- src/com/geekshow/controller/UsersController.java
+++ src/com/geekshow/controller/UsersController.java
@@ -6,9 +6,11 @@ import java.util.UUID;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -38,6 +40,21 @@ public class UsersController {
 			return "redirect:/showFile";
 		}else{
 			return "redirect:/register.jsp";
+		}
+	}
+	
+	@RequestMapping("{page}")
+	public String main(@PathVariable String page){
+		System.out.println("restful");
+		return page;
+	}
+	@RequestMapping("login")
+	public String login(Users users,HttpSession session){
+		if(users.getUsername().equals("admin")&&users.getPassword().equals("123")){
+			session.setAttribute("users", users);
+			return "main";
+		}else{
+			return "redirect:/login.jsp";
 		}
 	}
 }
